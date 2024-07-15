f89162da6acc7f22be6ed0ddebaa754cbc62ca93
Aline-Pinotti <alinefpinotti@gmail.com>
2024-07-15 13:29:18 -0300

    ADD classes Curso e Mentoria

---
+++ src/br/com/dio/desafio/Main.java
@@ -0,0 +1,22 @@
+package br.com.dio.desafio;
+
+import java.time.LocalDate;
+
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Curso curso1 = new Curso("curso Java", "descrição curso java", 8);
+		Curso curso2 = new Curso("curso JavaScript", "descrição curso java script", 4);
+		
+		System.out.println(curso1);
+		System.out.println(curso2);
+		
+		Mentoria mentoria = new Mentoria("mentoria de java", "descrição da mentoria de java", LocalDate.now());
+		System.out.println(mentoria);
+
+	}
+
+}
